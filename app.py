4ea364655dfa88037132ae26e94cb19110499ff7
RAJAT JANA <75692637+RajatJana@users.noreply.github.com>
2023-09-28 00:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,50 +25,52 @@ def run():
     ## Full Name
     fn = st.text_input('Full Name')
 
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
     ## For gender
     gen_display = ('Female','Male')
     gen_options = list(range(len(gen_display)))
     gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
-
+    with col1:
     ## For Marital Status
     mar_display = ('No','Yes')
     mar_options = list(range(len(mar_display)))
     mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
-
+    with col1:
     ## No of dependets
     dep_display = ('No','One','Two','More than Two')
     dep_options = list(range(len(dep_display)))
     dep = st.selectbox("Dependents",  dep_options, format_func=lambda x: dep_display[x])
-
+    with col2:
     ## For edu
     edu_display = ('Not Graduate','Graduate')
     edu_options = list(range(len(edu_display)))
     edu = st.selectbox("Education",edu_options, format_func=lambda x: edu_display[x])
-
+    with col2:
     ## For emp status
     emp_display = ('Job','Business')
     emp_options = list(range(len(emp_display)))
     emp = st.selectbox("Employment Status",emp_options, format_func=lambda x: emp_display[x])
-
+    with col2:
     ## For Property status
     prop_display = ('Rural','Semi-Urban','Urban')
     prop_options = list(range(len(prop_display)))
     prop = st.selectbox("Property Area",prop_options, format_func=lambda x: prop_display[x])
-
+    with col3:
     ## For Credit Score
     cred_display = ('Between 300 to 500','Above 500')
     cred_options = list(range(len(cred_display)))
     cred = st.selectbox("Credit Score",cred_options, format_func=lambda x: cred_display[x])
-
+    with col3:
     ## Applicant Monthly Income
     mon_income = st.number_input("Applicant's Monthly Income($)",value=0)
-
+    with col3:
     ## Co-Applicant Monthly Income
     co_mon_income = st.number_input("Co-Applicant's Monthly Income($)",value=0)
-
+    with col4:
     ## Loan AMount
     loan_amt = st.number_input("Loan Amount",value=0)
-
+    with col4:
     ## loan duration
     dur_display = ['2 Month','6 Month','8 Month','1 Year','16 Month']
     dur_options = range(len(dur_display))
